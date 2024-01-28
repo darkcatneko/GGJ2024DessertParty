cdb1c1041716dbecc5e658775359a29ddfd1413e
ginnerlee <ginner1206@gmail.com>
2024-01-28 12:25:52 +0800

    DessertDone

---
+++ Assets/scripts/CountdownTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameTimeUpEvent : UnityEvent { }
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float TotalTime = 60.0f;
+    private float CurrentTime;
+    public Text TimerText;
+
+    public GameTimeUpEvent OnGameTimeUp= new GameTimeUpEvent();
+
+
+    //void Start()
+    //{
+    //    CurrentTime = TotalTime;
+    //    Update TimerText();
+    //}
+
+    //void Update()
+    //{
+    //    if (currentTime > 0)
+    //    {
+    //        currentTime -= Time.deltaTime;
+    //        UpdateTimerText();
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("倒计时结束！");
+    //        OnGameTimeUp.Invoke(); 
+    //    }
+    //}
+}
