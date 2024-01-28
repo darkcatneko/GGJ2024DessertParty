f75c294c124b62d939bb6c0a0a99ab87edd02f24
ginnerlee <ginner1206@gmail.com>
2024-01-28 12:47:30 +0800

    Timeup

--- Assets/scripts/CountdownTimer.cs
+++ Assets/scripts/CountdownTimer.cs
@@ -16,23 +16,29 @@ public class CountdownTimer : MonoBehaviour
     public GameTimeUpEvent OnGameTimeUp= new GameTimeUpEvent();
 
 
-    //void Start()
-    //{
-    //    CurrentTime = TotalTime;
-    //    Update TimerText();
-    //}
+    void Start()
+    {
+        CurrentTime = TotalTime;
+        //TimerText = this.gameObject.GetComponent<Text>();
+        UpdatetimerText();
+    }
 
-    //void Update()
-    //{
-    //    if (currentTime > 0)
-    //    {
-    //        currentTime -= Time.deltaTime;
-    //        UpdateTimerText();
-    //    }
-    //    else
-    //    {
-    //        Debug.Log("倒计时结束！");
-    //        OnGameTimeUp.Invoke(); 
-    //    }
-    //}
+    void Update()
+    {
+        if (CurrentTime > 0)
+        {
+            CurrentTime -= Time.deltaTime;
+            UpdatetimerText();
+        }
+        else
+        {
+            OnGameTimeUp.Invoke();
+        }
+    }
+    void UpdatetimerText()
+    {
+        int minutes = Mathf.FloorToInt(CurrentTime / 60);
+        int seconds = Mathf.FloorToInt(CurrentTime % 60);
+        TimerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
